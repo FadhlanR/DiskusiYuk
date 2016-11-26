de17fda34ba24d4700dc2f882704c068017dc7f0
toxinoid00 <senzpmd@gmail.com>
2016-11-26 19:16:19 +0700

    add folder

---
+++ user/chat.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Welcome</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="./../css/chat.css">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="home.php">Home</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</head>
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-4 sidenav">
+      <h3><b>Topik</b></h4>
+      <h4>Admin: Topik</h4>
+      <h4>Rating: <img src="paris.jpg" alt="Paris" width="100" height="25"></h4>
+      <h4>Sumber asli: <a href="#">link</a></h4>
+    </div>
+
+    <div class="col-sm-6">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <ul class="chat">
+                        <li class="left clearfix"><span class="chat-img pull-left">
+                            <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p> 
+                            </div>
+                                <a class="like"><i class="fa fa-thumbs-o-up"></i>  
+                                    Like <input class="qty1" name="qty1" readonly="readonly" type="text" value="0" />
+                                </a>
+                                <a class="dislike"><i class="fa fa-thumbs-o-down"></i> 
+                                    Dislike <input class="qty2"  name="qty2" readonly="readonly" type="text" value="0" />
+                                </a>
+                                <a class="unrelevant"><i class="fa fa-thumbs-o-down"></i> 
+                                    Unrelevant <input class="qty4"  name="qty4" readonly="readonly" type="text" value="0" />
+                                </a>
+                        </li>
+                        <li class="right clearfix"><span class="chat-img pull-right">
+                            <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                    <strong class="pull-right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                                <a class="like"><i class="fa fa-thumbs-o-up"></i>  
+                                    Like <input class="qty1" name="qty1" readonly="readonly" type="text" value="0" />
+                                </a>
+                                <a class="dislike"><i class="fa fa-thumbs-o-down"></i> 
+                                    Dislike <input class="qty2"  name="qty2" readonly="readonly" type="text" value="0" />
+                                </a>
+                                <a class="unrelevant"><i class="fa fa-thumbs-o-down"></i> 
+                                    Unrelevant <input class="qty4"  name="qty4" readonly="readonly" type="text" value="0" />
+                                </a>
+                        </li>
+                        <li class="left clearfix"><span class="chat-img pull-left">
+                            <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                                
+                            </div>
+                            <a class="like"><i class="fa fa-thumbs-o-up"></i>  
+                                    Like <input class="qty1" name="qty1" readonly="readonly" type="text" value="0" />
+                                </a>
+                                <a class="dislike"><i class="fa fa-thumbs-o-down"></i> 
+                                    Dislike <input class="qty2"  name="qty2" readonly="readonly" type="text" value="0" />
+                                </a>
+                                <a class="unrelevant"><i class="fa fa-thumbs-o-down"></i> 
+                                    Unrelevant <input class="qty4"  name="qty4" readonly="readonly" type="text" value="0" />
+                                </a>
+                        </li>
+                        <li class="right clearfix"><span class="chat-img pull-right">
+                            <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>15 mins ago</small>
+                                    <strong class="pull-right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                            <a class="like"><i class="fa fa-thumbs-o-up"></i>  
+                                    Like <input class="qty1" name="qty1" readonly="readonly" type="text" value="0" />
+                                </a>
+                                <a class="dislike"><i class="fa fa-thumbs-o-down"></i> 
+                                    Dislike <input class="qty2"  name="qty2" readonly="readonly" type="text" value="0" />
+                                </a>
+                                <a class="unrelevant"><i class="fa fa-thumbs-o-down"></i> 
+                                    Unrelevant <input class="qty4"  name="qty4" readonly="readonly" type="text" value="0" />
+                                </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="panel-footer">
+                    <div class="input-group">
+                        <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                        <span class="input-group-btn">
+                            <button class="btn btn-warning btn-sm" id="btn-chat">
+                                Send</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  </div>
+</div>
+<footer class="container-fluid text-center">
+  <p>© 2016. Made with ❤ in Hackathon UMN by TEAM CEMARA </p>
+</footer>
+</body>
+</html>
---
+++ user/home.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Welcome</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!--<link rel="stylesheet" href="./../css/chat.css">-->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="home.php">Home</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</head>
+<body>
+<div class="container text-center">    
+  <div class="row">
+    <div class="col-sm-3 well">
+      <div class="well">
+        <p><a href="#">My Profile</a></p>
+        <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">
+      </div>
+    </div>
+    <div class="col-sm-7">
+    
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="panel panel-default text-left">
+            <div class="panel-body">
+              <h1>Welcome</h1>
+              <h3>Husain</h3>          
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-1">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Create</button>
+
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Create Room</h4>
+              </div>
+              <div class="modal-body">
+              <div class="col-sm-1">
+              <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Kategori
+                <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                  <li><a href="#">HTML</a></li>
+                  <li><a href="#">CSS</a></li>
+                  <li><a href="#">JavaScript</a></li>
+                </ul>
+                </div>
+              </div>
+              <div class="col-sm-6">
+              <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">List Topik
+                <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                  <li><a href="#">HTML</a></li>
+                  <li><a href="#">CSS</a></li>
+                  <li><a href="#">JavaScript</a></li>
+                </ul>
+                </div>
+              </div>
+              </div>
+              <br>
+              </br>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-default">Create</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Category
+        <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+          <li><a href="#">Islam</a></li>
+          <li><a href="#">Politic</a></li>
+          <li><a href="#">Technology</a></li>
+        </ul>
+      </div>
+    </div>
+      <form class="col-sm-8" role="search">
+          <div class="form-group input-group">
+            <input type="text" class="form-control" placeholder="Search..">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="button">
+                <span class="glyphicon glyphicon-search"></span>
+              </button>
+            </span>        
+          </div>
+        </form>
+    </div>
+
+      <div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Topik 1</a>
+      </h4>
+    </div>
+    <div id="collapse1" class="panel-collapse collapse in">
+      <div class="panel-body"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</p>
+      <p>admin: Admin</p>
+      <p>rating: <img src="paris.jpg" alt="Paris" width="100" height="25"></p>
+      <a href="chat.php"><button class="btn btn-primary">Join</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+        Topik 2</a>
+      </h4>
+    </div>
+    <div id="collapse2" class="panel-collapse collapse">
+      <div class="panel-body"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</p>
+      <p>admin: Admin</p>
+      <p>rating: <img src="paris.jpg" alt="Paris" width="100" height="25"></p>
+      <button class="btn btn-primary">Join</button>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+        Topik 3</a>
+      </h4>
+    </div>
+    <div id="collapse3" class="panel-collapse collapse">
+      <div class="panel-body"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</p>
+      <p>admin: Admin</p>
+      <p>rating: <img src="paris.jpg" alt="Paris" width="100" height="25"></p>
+      <button class="btn btn-primary">Join</button>
+      </div>
+    </div>
+  </div>
+</div>   
+    </div>
+    <div class="col-sm-2 well">
+        <p></p>
+        <p><strong>TOP DISCUSSION</strong></p>
+        <p>Lorem ipsum ...</p>
+        <p>admin: Admin</p>
+        <p>rating: <img src="paris.jpg" alt="Paris" width="100" height="25"></p>
+        <button class="btn btn-primary">Join</button>
+        <p></p>     
+    </div>
+
+  </div>
+</div>
+<!-- Trigger the modal with a button -->
+<style>    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+</style>
+<footer class="container-fluid text-center">
+  <p>© 2016. Made with ❤ in Hackathon UMN by TEAM CEMARA </p>
+</footer>
+</body>
+</html>
