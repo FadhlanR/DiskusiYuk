3d8672967ef159fc16534d74bede8f736e2779ed
FadhlanR <fridhwanallah@gmail.com>
2016-11-26 23:20:34 +0700

    Modified admin by fadhlan ver 2

---
+++ admin/monitoring.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <div class="col-md-9">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="content-box-large">
+                        <div class="panel-heading">
+                            <div class="panel-title"><?php echo $_GET['page']?></div>
+
+                            <div class="panel-options">
+                                <a href="index.php?page=<?php echo $_GET['page']?>" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+                                <a href="index.php?page=<?php echo $_GET['page']?>" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <table class="table table-bordered">
+                              <tr>
+                                <th>
+                                  <?php
+                                  if($_GET['page']=="Dislike Opinion"){echo 'Total Dislike';}
+                                  else if($_GET['page']=="Sentiment Analysis"){echo 'Sentiment Score';}
+                                  else if($_GET['page']=="Irrelevant Opinion"){echo 'Irrelevant Score';} ?>
+                                </th>
+                                <th>Member</th>
+                                <th>Time</th>
+                                <th>Comment</th>
+                                <th></th>
+                                <th></th>
+                              </tr>
+                              <?php
+                                if(isset($id_discussion_forum)){
+                                  if($_GET['page']=="Dislike Opinion"){
+                                    $sentimen = $db->prepare("SELECT * FROM comments c, user u, members m WHERE c.id_discussion_forum = :id AND c.id_user = u.id_user AND c.id_user = m.id_user and m.allowed = 0 ORDER BY c.dislike_comment desc");
+                                    $poin = 'dislike_comment';
+                                  }
+                                  else if($_GET['page']=="Sentiment Analysis"){
+                                    $sentimen = $db->prepare("SELECT * FROM comments c, user u, members m WHERE c.id_discussion_forum = :id AND c.id_user = u.id_user AND c.id_user = m.id_user and m.allowed = 0 ORDER BY c.sentiment_score desc");
+                                    $poin = 'sentiment_score';
+                                  }
+                                  else if($_GET['page']=="Irrelevant Opinion"){
+                                    $sentimen = $db->prepare("SELECT * FROM comments c, user u, members m WHERE c.id_discussion_forum = :id AND c.id_user = u.id_user AND c.id_user = m.id_user and m.allowed = 0 ORDER BY c.unrelated_comment desc");
+                                    $poin = 'unrelated_comment';
+                                  }
+                                  $sentimen->bindValue(':id',$id_discussion_forum);
+                                  $sentimen->execute();
+                                  $sentimen = $sentimen->fetchAll();
+                                  foreach ($sentimen as $key => $value) {
+                                    echo '<tr>
+                                            <td>'.$value[$poin].'</td>
+                                            <td>'.$value['nick_name'].'</td>
+                                            <td>'.$value['added'].'</td>
+                                            <td>'.$value['comment'].'</td>
+                                            <td><button>Delete</button></td>
+                                            <td><button>Block</button></td>
+                                          </tr>';
+                                  }
+                                }
+                                ?>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</html>
